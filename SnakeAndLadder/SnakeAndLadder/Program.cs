69429e9ef4db15b3a864ebd34d13c1fcaceb2cba
Praan26 <arpanap102@gmail.com>
2022-04-08 23:35:28 +0530

    [Arpan] Add: single player game added

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SnakeAndLadder;
+SnakeGame Game = new SnakeGame();
+int num = Game.DieRoll();
+Console.WriteLine(num);
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeGame
+    {
+        int position = 0;
+        public int DieRoll()
+        {
+            Random random = new Random();
+            int diePosition = random.Next(1, 7);
+            return diePosition;
+        }
+    }
+}
